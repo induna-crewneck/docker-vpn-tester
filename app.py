3bd812f070d86b1dd5114035729e223f746202c0
induna-crewneck <66157637+induna-crewneck@users.noreply.github.com>
2025-10-29 14:06:30 +0100

    Upgrade v2.0
    Improved error handling for IP retrieval and included icanhazip as fallback for ipinfo rate limitation

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-# Docker VPN Checker v1.1
+# Docker VPN Checker v2.0
 # https://github.com/induna-crewneck/docker-vpn-tester
 # Developed and tested on Raspberry Pi OS x64 / Debian 1:6.6.20-1+rpt1 (2024-03-07)
 
@@ -13,9 +13,14 @@ def get_ip_and_location():
         data = response.json()
         ip = data.get("ip", "N/A")
         location = f'{data.get("city", "N/A")}, {data.get("region", "N/A")}, {data.get("country", "N/A")}'
+        if "N/A, N/A, N/A" in location and ip == "N/A":
+            response2 = requests.get("https://ipv4.icanhazip.com")
+            ip = response2.text.replace('\n', '')
+            location = "N/A (ipinfo rate likely exceeded)"
         return ip, location
     except Exception as e:
-        return "N/A", "N/A"
+        print(f"Exception: {e}")
+        return "N/A", f"N/A ({e})"
 
 def log_ip_and_location():
     ip, location = get_ip_and_location()
@@ -23,7 +28,9 @@ def log_ip_and_location():
     sys.stdout.write(f"[{timestamp}] Public IP:   {ip}    {location}\n")
     sys.stdout.flush()
     
-sys.stdout.write(f"Starting up...")
+timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+sys.stdout.write(f"[{timestamp}] Starting up...\n")
+
 if __name__ == "__main__":
     while True:
         log_ip_and_location()
